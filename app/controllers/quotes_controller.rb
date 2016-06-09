2003d327e19181efcf486c0ed3dbfbc36e539b9d
Melissa Brown <melissabrown861@gmail.com>
2016-06-09 04:17:56 +0000

    added about page

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -7,13 +7,16 @@ class QuotesController < ApplicationController
         @quote = Quote.new
     end   
     
-  def create
-    @quote = Quote.create(quote_params)
-    if @quote.invalid?
-      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    def create
+      @quote = Quote.create(quote_params)
+        if @quote.invalid?
+          flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+        end
+      redirect_to root_path
     end
-    redirect_to root_path
-  end
+      
+    def about
+    end      
      
     private 
     
